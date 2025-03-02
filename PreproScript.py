7cb383b28c7a787cbd1a58b344b6b2045df4605d
Primarizki21 <primarizkiahmad.h@gmail.com>
2025-03-02 16:58:56 +0700

    feature 1, finalisasi

--- PreproScript.py
+++ PreproScript.py
@@ -1,5 +1,6 @@
 import typer
 import pandas as pandas
+import re
 
 app = typer.Typer()
 
@@ -9,13 +10,13 @@ def apakah_kolom_id(kolom):
     Pola kata dari kolom yang ingin dihapus
     Mencari kolom dengan pola kata "ID", "customer", dan "tax"
     """
-    pattern = r'(id|customer|tax)[_A-Z]?' 
+    pattern = r'(id)[_A-Z]?' 
     return re.search(pattern, kolom, re.IGNORECASE)
 
-def identifikasi_kolom_id(df, threshold = 0.85):
+def identifikasi_kolom_id(df, threshold = 0.7):
     """
     Identifikasi Kolom ID
-    Menggunakan threshold 0.85 sebagai batas nilai Kardinalitas
+    Menggunakan threshold 0.7 sebagai batas nilai Kardinalitas
     """
     kolom_id = []
     for kolom in df.columns:
@@ -23,11 +24,19 @@ def identifikasi_kolom_id(df, threshold = 0.85):
         if (ratio >= threshold) and apakah_kolom_id(kolom):
             kolom_id.append(kolom)
     return kolom_id
-    
-@app.command()
-def clean():
-    pass
 
+@app.command()
+def clean(input_file: str):
+    try:
+        df = pd.read_csv(input_file)
+
+        # 1. Menghapus Kolom ID
+        kolom_id = identifikasi_kolom_id(df)
+        df = df.drop(columns = kolom_id)
+        
+    except Exception as e:
+        typer.secho(f"Error: {str(e)}", fg=typer.colors.RED)
+        raise typer.Exit(code=1)
 
 if __name__ == "__main__":
     app()
